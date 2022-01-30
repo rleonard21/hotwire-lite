0ae7f8b6d162416fc85631433cda075293a2f6c6
rleonard21 <bl.cirrus@gmail.com>
2022-01-30 01:32:00 -0500

    added time analysis of skipping average

--- dsp/SkippingAverage.py
+++ dsp/SkippingAverage.py
@@ -1,12 +1,19 @@
 class SkippingAverage:
-	def __init__(self, bit_size):
+	def __init__(self, bit_size, noise_floor, simple=False):
 		self.bit_size = bit_size
 		self.size = 1 << bit_size # 2^n
+		self.noise_floor = noise_floor
 		
 		self.data = [0] * self.size
 		self.average = 0
 
+		self.simple_sma = simple
+
 	def out(self, sample):
+		# skip samples that are less than the noise
+		if sample <= self.noise_floor and not self.simple_sma:
+			return self.average >> self.bit_size
+
 		self.data.append(sample)
 
 		if len(self.data) == self.size + 1:
